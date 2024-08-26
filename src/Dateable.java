9615a57a19be3cd2b08b0e596277cfb9156b3400
isabellang <isabellayln2027@student.cis.edu.hk>
2024-08-26 09:00:14 +0800

    Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int yourAge = age.nextInt();
+        System.out.println(yourAge + " year olds should date somebody at least " + (7+(yourAge/2)) + " years old");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int yourAge = age.nextInt();
+        System.out.println(yourAge+ " years old should read at least " + (100-yourAge)+ " pages before giving up on a book");
     }
 }
